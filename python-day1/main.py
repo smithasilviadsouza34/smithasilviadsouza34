fc3057f8923c87e68d53a2bdc8509e182aee4ee3
smithasilviadsouza34 <145434526+smithasilviadsouza34@users.noreply.github.com>
2023-09-19 20:13:47 +0530

    day 1 of python learning

---
+++ python-day1/main.py
@@ -0,0 +1,9 @@
+print("Smitha Silvia Dsouza")
+print("19-09-2023")
+print("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+""")
+print("I am feeling 😍")
+print("You can follow my progress at replit.com/@smithadsouza1")
